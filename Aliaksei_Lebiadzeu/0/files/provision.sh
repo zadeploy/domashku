9926b6959cf4b0067a113c0fec2fc9dea81ac4de
Aliaksei Lebiadzeu <alexey@MacBook-Pro-Aliaksei.local>
2019-12-07 20:05:17 +0300

    Fix problem with infinite-loop

--- Aliaksei_Lebiadzeu/0/files/provision.sh
+++ Aliaksei_Lebiadzeu/0/files/provision.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+ps aux | grep infinite-loop 
+sudo killall -r infinite-loop # kill current infinite-loop processes
+sudo update-rc.d -f infinite-loop remove &2>/dev/null # unregister infinite-loop from autostart
+sudo rm /etc/init.d/infinite-loop # remove file with infinite-loop script
 cp /tmp/site.conf /etc/apache2/sites-available/site.conf > /dev/null
 a2ensite site.conf > /dev/null
 
