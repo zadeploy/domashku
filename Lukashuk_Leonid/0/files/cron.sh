27ac4421140eaf7e35f0f26e38a856396aa7a6c9
Leonid Lukashuk <l.lukashuk@glera.local>
2019-12-10 22:24:28 +0300

    Task 0

--- Lukashuk_Leonid/0/files/cron.sh
+++ Lukashuk_Leonid/0/files/cron.sh
@@ -1,4 +1,4 @@
 apt-get install cron -y
-echo "* * * * * ./exercise-memcached.sh" | crontab - -u vagrant
+echo "*/1 * * * * ./exercise-memcached.sh" | crontab - -u vagrant
 service cron restart
 echo "exercise-memcached.sh cronjob start"
