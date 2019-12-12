731f3bf9d5ad276824b0bb8ec5e6827669a3798a
Artyom <artyom021997@gmail.com>
2019-12-12 19:06:13 +0300

    devops-test

--- _hw#0/files/provision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp /tmp/site.conf /etc/apache2/sites-available/site.conf > /dev/null
-a2ensite site.conf > /dev/null
-
-echo "Hi, I don't do anything right now. I'm just an example of how to do basic provisioning with shell scripts."
-service apache2 stop  1>&2 > /dev/null  # I lied
