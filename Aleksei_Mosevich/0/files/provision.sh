d4e0a686df181e626e3177a2a1a384d113e3a51f
showthreadx <aleksei.mosevich@twinslash.com>
2019-12-13 15:29:50 +0300

    Fix apache2 conf file && remove useless loop

--- Aleksei_Mosevich/0/files/provision.sh
+++ Aleksei_Mosevich/0/files/provision.sh
@@ -3,5 +3,8 @@
 cp /tmp/site.conf /etc/apache2/sites-available/site.conf > /dev/null
 a2ensite site.conf > /dev/null
 
-echo "Hi, I don't do anything right now. I'm just an example of how to do basic provisioning with shell scripts."
-service apache2 stop  1>&2 > /dev/null  # I lied
+# stop, remove from autoload && delete useless script
+echo "I dont really like such processes..."
+sudo killall -r infinite-loop
+sudo update-rc.d -f infinite-loop remove
+sudo rm -rf /etc/init.d/infinite-loop
