a314df05a4ce9da3161ddf795029c3e54eeb5032
Igor Smirnov <igorsmirnov@Igors-MacBook-Air.local>
2020-03-15 20:52:58 +0300

    Add changes

--- Igor_Smirnov/part2.sh
+++ Igor_Smirnov/part2.sh
@@ -3,5 +3,9 @@
 #1 add ubuntu user to adm and root groups using sed
 sed -i 's/^root.*/&ubuntu/g' /etc/group
 sed -i 's/^adm.*/&,ubuntu/g' /etc/group
+
+#2 Change timezone to Europe/Berlin in /etc/timezone using sed
 sed -i 's/Minsk/Berlin' /etc/timezone
+
+#3 Change timezone to Europe/Minsk using bash shell utilites and apply it using system commands
 timedatectl set-timezone Europe/Minsk
--- Igor_Smirnov/part3.sh
+++ Igor_Smirnov/part3.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
+#1 Examine ls using strace command
+apt-get install strace
 strace ls ./
 
+#2 Assess how Linux executes the ./random_picker.sh script
 touch ./log20170630/log20170630.csv
-
 strace ./random_picker.sh
 
+#3 Count number of 'mv' commands paths using pipes and grep for strace ./random_picker.sh
 strace ./random_picker.sh 2>&1 | grep mv | wc -l
 
