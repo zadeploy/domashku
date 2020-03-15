c450532ec2cd55f092245753f9f60ab7ae6e4780
Dmitry_Predko <58135000+qwe765@users.noreply.github.com>
2020-03-15 18:00:52 +0300

    Update part2.sh

--- Dmitry_Predko/Hometask 2: Shell/part2.sh
+++ Dmitry_Predko/Hometask 2: Shell/part2.sh
@@ -3,5 +3,10 @@
 echo "Enter username"
 read USER
 sed -i "/adm:/s/$/,$USER/; /root:/s/$/,$USER/" /etc/group
-
 echo "User $USER added to groups ADM and ROOT"
+
+sed -i '1c\Europe/Berlin' /etc/timezone
+echo "The timezone changed to Europe/Berlin"
+
+timedatectl set-timezone Europe/Minsk
+echo "The timezone changed to Europe/Minsk"
