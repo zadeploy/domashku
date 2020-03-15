68f8989b936a5e208017010d91d2a2575bece965
Anatoli Babenia <anatoli@rainforce.org>
2020-03-15 17:25:57 +0300

    Download and unpack EDGAR log

---
+++ abitrolly/2/process.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Download http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2017/Qtr2/log20170630.zip using shell
+URL="http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2017/Qtr2/log20170630.zip"
+
+curl -#LO "$URL"
+# -#  sane progress bar
+# -L  follow redirects
+# -O  download as a file
+
+
+# Unpack the log file
+unzip "$(basename $URL)"
+
+
+# Change owner of log file to your current user using chown
+
+# Change set executive bit to a random.sh script using chmod
+
+# Execute the random.sh script
+
+# Find the file using find
+
+# Try to look for errors using your favorite editor
+
+# Try to find errors using grep
+
+# Tail last 500 lines of file
+
+# Find how many index.htm hits were at 30.06.2017 14:00
+
+# Find how many index.htm hits were at 30.06.2017 17:00-18:00
+
+# Show the number of times each IP shows up in the log – using sort and uniq utilities
+
+# Count all 13.94.212.jdf IP hits us
