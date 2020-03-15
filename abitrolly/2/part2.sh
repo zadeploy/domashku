1be12dfb4122ff4644dedaa0cd738754246d44b7
Anatoli Babenia <anatoli@rainforce.org>
2020-03-15 23:20:06 +0300

    Change timezone to Europe/Berlin in /etc/timezone using sed

--- abitrolly/2/part2.sh
+++ abitrolly/2/part2.sh
@@ -5,5 +5,6 @@ sed -i -r -e '/ubuntu/! s/^(root|adm):.*\w$/\0,/'  `# add comma if needed` \
 	  -e '/ubuntu/! s/^(root|adm):.*/\0ubuntu/' /etc/group
 
 : Change timezone to Europe/Berlin in /etc/timezone using sed
+sed -i 's/.*/Europe\/Berlin/' /etc/timezone
 
 : Change timezone to Europe/Minsk using bash shell utilites and apply it using system commands
