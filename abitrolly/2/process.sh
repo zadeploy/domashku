7810aefa93fe52584155a28ec02eb0608b60c3d9
Anatoli Babenia <anatoli@rainforce.org>
2020-03-15 22:15:51 +0300

    Adapt for scripting. Use silent curl, commented vim and : comments
        sudo ./process.sh |& tee output.txt

--- abitrolly/2/process.sh
+++ abitrolly/2/process.sh
@@ -4,54 +4,55 @@ set -x
 set -o errexit
 set -o nounset
 
-# Download http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2017/Qtr2/log20170630.zip using shell
+: Download http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2017/Qtr2/log20170630.zip using shell
 URL="http://www.sec.gov/dera/data/Public-EDGAR-log-file-data/2017/Qtr2/log20170630.zip"
 FILENAME="$(basename $URL)"
 NAME="$(basename $FILENAME .zip)"
 
 # download only if there is no file
-[[ -f $FILENAME ]] || curl -#LO "$URL"
-# -#  sane progress bar
-# -L  follow redirects
+[[ -f $FILENAME ]] || curl -sSLO "$URL"
+# -s  silent
+# -S  hill
+# -L  hollow redirects
 # -O  download as a file
 
 
-# Unpack the log file
+: Unpack the log file
 CSVNAME="$NAME/$NAME.csv"
 [[ -f $CSVNAME ]] || unzip -d "$NAME" "$FILENAME"
 echo "Unpacked"
 
 
-# Change owner of log file to your current user using chown
+: Change owner of log file to your current user using chown
 chown $USER $CSVNAME
 
-# Change set executive bit to a random.sh script using chmod
+: Change set executive bit to a random.sh script using chmod
 chmod +x random.sh
 
-# Execute the random.sh script
+: Execute the random.sh script
 ./random.sh
 
-# Find the file using find
+: Find the file using find
 NEWLOC=$(find /usr -type f -name "$NAME.csv")
 
-# Try to look for errors using your favorite editor
-vim "$NEWLOC"
+: Try to look for errors using your favorite editor
+#vim "$NEWLOC"
 
-# Try to find errors using grep
+: Try to find errors using grep
 grep 404 "$NEWLOC" -m 10
 
-# Tail last 500 lines of file
+: Tail last 500 lines of file
 tail -n 500 "$NEWLOC"
 
-# Find how many index.htm hits were at 30.06.2017 14:00
+: Find how many index.htm hits were at 30.06.2017 14:00
 grep "2017-06-30,14:00" "$NEWLOC" | grep "index.htm" | wc -l
 
-# Find how many index.htm hits were at 30.06.2017 17:00-18:00
+: Find how many index.htm hits were at 30.06.2017 17:00-18:00
 grep "2017-06-30,17" "$NEWLOC" | grep "index.htm" | wc -l
 
-# Show the number of times each IP shows up in the log – using sort and uniq utilities
+: Show the number of times each IP shows up in the log – using sort and uniq utilities
 cut -d, -f1 < "$NEWLOC" | sort | uniq -c | sort -rn
 
-# Count all 13.94.212.jdf IP hits us
+: Count all 13.94.212.jdf IP hits us
 grep "13.94.212.jdf" "$NEWLOC" | wc -l
 
