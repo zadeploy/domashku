22b32fbca9b8a59290ac72ff906c38c42e328291
Dmitry_Predko <58135000+qwe765@users.noreply.github.com>
2020-03-18 18:26:34 +0300

    Update addCloudflareDNS.sh

--- Dmitry_Predko/Hometask 3: Shell Scripts/addCloudflareDNS.sh
+++ Dmitry_Predko/Hometask 3: Shell Scripts/addCloudflareDNS.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+IP1=`dig @1.1.1.1 arya.ns.cloudflare.com +short`
+IP2=`dig @1.1.1.1 chance.ns.cloudflare.com +short`
+echo -e "arya.ns.cloudflare.com $IP1 \nchance.ns.cloudflare.com $IP2" | tee -a /etc/resolv.conf
+~                                                                                                          
