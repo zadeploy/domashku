c88a03342dccdd240d1724cd6fe39f88e0835318
Dmitry_Predko <58135000+qwe765@users.noreply.github.com>
2020-03-21 15:42:33 +0300

    Update registerRESTAPI.sh

--- Dmitry_Predko/Hometask 3: Shell Scripts/registerRESTAPI.sh
+++ Dmitry_Predko/Hometask 3: Shell Scripts/registerRESTAPI.sh
@@ -28,17 +28,17 @@
 
 IP=`dig @arya.ns.cloudflare.com $HOSTNAME.lab.zadeploy.com +short`
 LOCIP=`hostname -i | cut -d' ' -f1`
+ID=
 
 if [ $? -eq 0 ]; then
         echo "The record $HOSTNAME.lab.zadeploy.com already exists with ip = $IP "
         if [ "$IP" == "$LOCIP" ]; then
                 echo "The A-record is up to date"
         else
-              curl -X PUT "https://api.cloudflare.com/client/v4/zones/023e105f4ecef8ad9ca31a8372d0c353/dns_records/372e67954025e0ba6aaa6d586b9e0b59" \
-                        -H "X-Auth-Email: user@example.com" \
-                        -H "X-Auth-Key: c2547eb745079dac9320b638f5e225cf483cc5cfdda41" \
-                        -H "Content-Type: application/json" \
-                        --data '{"type":"A","name":"example.com","content":"127.0.0.1","ttl":120,"proxied":false}'
+              curl -X PUT "https://api.cloudflare.com/client/v4/zones/4f70d62b382c30c7f83942a758ed9eac/dns_records/$ID" \
+                -H "Authorization: Bearer gqAEvi3cCcEmWtsyjAVu_VFE8kfy58TYNHQPatd5"\
+                -H "Content-Type: application/json" \
+                --data '{"type":"A","name":"'"$HOSTNAME"'.lab.zadeploy.com","content":"'"$LOCIP"'","ttl":120,"proxied":false}'
         fi;
 else
         `curl -X POST "https://api.cloudflare.com/client/v4/zones/4f70d62b382c30c7f83942a758ed9eac/dns_records" \
