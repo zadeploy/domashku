63555964344483a975707342e8db0ba0a6d654d4
Dmitry_Predko <58135000+qwe765@users.noreply.github.com>
2020-03-23 16:34:10 +0300

    Update addCloudflareDNS.sh

--- Dmitry_Predko/Hometask 3: Shell Scripts/addCloudflareDNS.sh
+++ Dmitry_Predko/Hometask 3: Shell Scripts/addCloudflareDNS.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+DNS1=arya.ns.cloudflare.com
+DNS2=chance.ns.cloudflare.com
+
 IP1=`dig @1.1.1.1 arya.ns.cloudflare.com +short`
 IP2=`dig @1.1.1.1 chance.ns.cloudflare.com +short`
-echo -e "arya.ns.cloudflare.com $IP1 \nchance.ns.cloudflare.com $IP2" | tee -a /etc/resolv.conf                  
+
+for i in $DNS1 $DNS2
+do
+        grep -o $i /etc/resolv.conf > /dev/null
+        if [ $? -eq 0 ]; then
+                if [ $i == $DNS1 ]; then
+                        if [ `grep $i /etc/resolv.conf | cut -d" " -f2` == $IP1 ]; then
+                                echo "$DNS1 is OK"
+                        else
+                                sed -i 's/'"$DNS1"'.*/'"$DNS1 $IP1"'/g' /etc/resolv.conf
+                        fi
+                else
+                        if [ `grep $i /etc/resolv.conf | cut -d" " -f2` == $IP2 ]; then
+                                echo "$DNS2 is OK"
+                        else
+                                sed -i 's/'"$DNS2"'.*/'"$DNS2 $IP2"'/g' /etc/resolv.conf
+                        fi
+                fi
+        else
+                if [ $i == $DNS1 ]; then
+                        echo -e "$DNS1 $IP1" >> /etc/resolv.conf
+                else
+                        echo -e "$DNS2 $IP2" >> /etc/resolv.conf
+                fi
+        fi
+done
+                
