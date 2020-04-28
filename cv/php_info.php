f23baed727e4a95b24cfc0205572736a9789a5ea
Ladutko Sergey <s.ladutko@pixelplex.io>
2020-04-28 22:48:47 +0300

    [Hometask_5] nginx + CV

---
+++ cv/php_info.php
@@ -0,0 +1,5 @@
+curl -I -H 'Accept-Encoding: gzip,deflate' http://0.0.0.0:1111/ | grep gzip
+
+curl -I -H 'Accept-Encoding: gzip,deflate' http://0.0.0.0:1111/ | grep gzip
+
+curl --header "Accept-Encoding: gzip,deflate,sdch" -I http://cv.sergeyladutko.xyz
